81a8545c47635b7749faaacdcae55b9d4b762532
Dmytro O <mr.oda.ua@gmail.com>
2020-05-18 18:23:17 +0300

    Task 3 finished

--- js/task03.js
+++ js/task03.js
@@ -1,6 +1,12 @@
 // ===--- Task Description
 
-// Напиши функцию findBestEmployee(employees), которая принимает объект сотрудников и возвращает имя самого продуктивного (который выполнил больше всех задач). Сотрудники и кол-во выполненых задач содержатся как свойства объекта в формате "имя":"кол-во задач".
+// Напиши класс Storage, который будет создавать объекты для управления складом товаров. При вызове будет получать один аргумент - начальный массив товаров, и записывать его в свойство items.
+
+// Добавь методы класса:
+
+// getItems() - возвращает массив текущих товаров
+// addItem(item) - получает новый товар и добавляет его к текущим
+// removeItem(item) - получет товар и, если он есть, удаляет его из текущих
 
 // ===--- Task Description
 
@@ -8,50 +14,44 @@
 // ====---- Task 03 Start
 console.log("TASK 03");
 
-const findBestEmployee = function (employees) {
-  // твой код
-
-  let bestEmployee = Object.keys(employees)[0]; // let bestEmployee = the first one (any)
-
-  for (const key in employees) {
-    //console.log(key, employees[key]);
-
-    //compare the rate of the Best empl and the next:
-    if (employees[key] > employees[bestEmployee]) {
-      bestEmployee = key;
-    } // let the Best shall be the current
+class Storage {
+  constructor (itemsArray) {
+    this.items = itemsArray;
   }
-  return bestEmployee; // return the best one
-};
 
-/*
- * Вызовы функции для проверки работоспособности твоей реализации.
- */
-console.log(
-  "Best employee: ",
-  findBestEmployee({
-    ann: 29,
-    david: 35,
-    helen: 1,
-    lorence: 99,
-  })
-); // lorence
+  getItems() {
+    console.log(this.items);
+  }
 
-console.log(
-  "Best employee: ",
-  findBestEmployee({
-    poly: 12,
-    mango: 17,
-    ajax: 4,
-  })
-); // mango
+  addItem(item) {
+    this.items.push(item);
+  }
 
-console.log(
-  "Best employee: ",
-  findBestEmployee({
-    lux: 147,
-    david: 21,
-    kiwi: 19,
-    chelsy: 38,
-  })
-); // lux
+  removeItem(item) {
+    for (let i = 0; i < this.items.length; i += 1) {
+      // console.log(itemN);
+      if (this.items[i] === item) {
+        this.items.splice(i,1);
+      }
+    }
+  //  this.items.pull(item);
+  }
+
+}
+
+
+const storage = new Storage([
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор',
+]);
+
+const items = storage.getItems();
+console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+storage.addItem('Дроид');
+console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+storage.removeItem('Пролонгер');
+console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
